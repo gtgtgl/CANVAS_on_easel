8e43fa424305720a2174e776839bdbe2a66caf47
gtgtgl <ykr20141101@outlook.jp>
2020-03-31 14:44:56 +0900

    1.0.1

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
-<?php //子テーマで利用する関数を書く
-
+<?php
+// 親テーマのCSSを読み込む
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/library/css/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/library/css/ie.css' );
 }
+
+function parent_editor_style() {
+    add_editor_style( get_template_directory_uri() . '/library/css/editor-style.css' );
+}
+add_action( 'after_setup_theme', 'parent_editor_style' );
 
 //子テーマのスタイルシートを有効にする
 add_action( 'wp_enqueue_scripts', 'canvas_scripts_and_styles' );
@@ -13,5 +19,8 @@ function canvas_scripts_and_styles() {
 		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), '', 'all' );
 	}
 }
+
+// PHPを追記する場合はここより下に書いてください。
+
 
 ?>
