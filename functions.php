64aa63c75188584f95253f0f4403d6aa6bbb59e3
gtgtgl <ykr20141101@outlook.jp>
2020-05-21 12:01:04 +0900

    1.1.0

--- functions.php
+++ functions.php
@@ -1,24 +1,10 @@
 <?php
-// 親テーマのCSSを読み込む
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/library/css/style.css' );
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/library/css/ie.css' );
-}
-
-function parent_editor_style() {
-    add_editor_style( get_template_directory_uri() . '/library/css/editor-style.css' );
-}
-add_action( 'after_setup_theme', 'parent_editor_style' );
-
-//子テーマのスタイルシートを有効にする
-add_action( 'wp_enqueue_scripts', 'canvas_scripts_and_styles' );
+// 子テーマのスタイルシートを有効にする
 function canvas_scripts_and_styles() {
-  global $wp_styles;
-  if (!is_admin()) {
-		wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), '', 'all' );
-	}
+	wp_register_style( 'canvas-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), '', 'all' );
+  wp_enqueue_style( 'canvas-stylesheet' );
 }
+add_action( 'wp_enqueue_scripts', 'canvas_scripts_and_styles', 1000 );
 
 // PHPを追記する場合はここより下に書いてください。
 
